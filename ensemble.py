c1e34b2bc6110c594a9102ddca122fa142aa5475
Null-op <1142983747@qq.com>
2017-12-22 01:38:29 +0800

    experiment3

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,6 @@
 import pickle
+import numpy as np
+from sklearn import tree
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,21 +12,42 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
+        self.weak_classifier = weak_classifier
+        self.n_weakers_limit = n_weakers_limit
+        self.classifiers = list()
+        self.a = list()
         pass
 
     def is_good_enough(self):
         '''Optional'''
         pass
 
-    def fit(self,X,y):
+    def fit(self, X, y):
         '''Build a boosted classifier from the training set (X, y).
 
         Args:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
-
+        w = [1/len(X)]*len(X)
+        for m in range(self.n_weakers_limit):
+            print('start fitting the ' + str(m) + " classifier")
+            self.classifiers.append(self.weak_classifier(max_depth=3))
+            self.classifiers[m].fit(X, y, w)
+            print('start predicting the ' + str(m) + " classifier")
+            predicts = self.classifiers[m].predict(X)
+            em = 0
+            for i in range(len(predicts)):
+                if predicts[i] != y[i][0]:
+                    em += w[i]
+            em = max(em, 1e-16)
+            am = 0.5 * np.log((1-em) / em)
+            self.a.append(am)
+            zm = 0
+            for i in range(len(X)):
+                zm += w[i] * np.exp(-1 * am * (y[i][0]) * (predicts[i]))
+            for i in range(len(X)):
+                w[i] = (w[i]/zm)*np.exp(-1 * am * y[i][0] * predicts[i])
 
     def predict_scores(self, X):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
@@ -35,10 +58,19 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
+        result = [[None] for i in range(len(X))]
+        for index in range(len(X)):
+            score = 0
+            for m in range(self.n_weakers_limit):
+                batch = [X[index]]
+                _ = (self.classifiers[m].predict(batch))[0]
+                score += self.a[m] * _
+            result[index][0] = score
+        return result
         pass
 
     def predict(self, X, threshold=0):
-        '''Predict the catagories for geven samples.
+        '''Predict the catagories for given samples.
 
         Args:
             X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
@@ -46,7 +78,16 @@ class AdaBoostClassifier:
 
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
+
         '''
+        scores = self.predict_scores(X)
+        results = [[None] for i in range(len(scores))]
+        for i in range(len(scores)):
+            if scores[i][0] >= threshold:
+                results[i][0] = 1
+            else:
+                results[i][0] = -1
+        return results
         pass
 
     @staticmethod
--- train.py
+++ train.py
@@ -1,6 +1,75 @@
+from PIL import Image
+import pickle
+import os
+from feature import NPDFeature
+import numpy as np
+from ensemble import AdaBoostClassifier
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+# parameter
+use_cache = True
+
+
+def get_path(path):
+    # 给路径添加前缀
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg')]
+
+
+def conver_image(paths):
+    ims = list()
+    i = 0
+    for path in paths:
+        im = Image.open(path)
+        im = im.convert(mode='L')
+        im.thumbnail(size=(24, 24))
+        im = np.asarray(im)
+        im = NPDFeature(im)
+        im = NPDFeature.extract(im)
+        print(i)
+        i +=1
+        ims.append(im)
+    return ims
 
 
 if __name__ == "__main__":
-    # write your code here
-    pass
+    if os.stat("cache").st_size> 1024 and use_cache  :
+        # 已有缓存
+        with open('cache', mode='rb') as cache:
+            dataset = pickle.load(cache)
+        print("load cache done")
+    else:
+        # 没有缓存
+        faces_path = get_path('./datasets/original/face')
+        nonface_path = get_path('./datasets/original/nonface')
+        dataset = dict()
+        dataset['face'] = conver_image(faces_path)
+        dataset['nonface'] = conver_image(nonface_path)
+        with open('cache', mode='wb') as cache:
+            pickle.dump(dataset, file=cache)
+            print('save cache done')
 
+    # conduct x_train, x_test, y_train, y_test
+    y_true = [[1]] * len(dataset['face'])
+    y_false = [[-1]] * len(dataset['nonface'])
+    X = dataset['face'] + dataset['nonface']
+    Y = y_true + y_false
+    x_train, x_test, y_train, y_test = train_test_split(X, Y)
+    print("conduct train set and testing set done")
+
+    # fit
+    classsfier = AdaBoostClassifier(tree.DecisionTreeClassifier, 5)
+    classsfier.fit(x_train, y_train)
+
+    # predict
+    y_predict = classsfier.predict(x_test)
+
+
+    # conduct report
+    y_truth = [i[0] for i in y_test]
+    y_pred = [i[0] for i in y_predict]
+    report = classification_report(y_truth, y_pred)
+    with open('report.txt', 'w') as report_file:
+        report_file.write(report)
+    print(report)
